5d25e5b8c32fce7e1e0d67ced9489b58090cb000
Aleksandr Kovalko <gistrec@gmail.com>
2025-09-07 14:25:40 +0200

    Init

---
+++ GlucoseSync/GlucoseSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlucoseSyncApp.swift
+//  GlucoseSync
+//
+//  Created by Aleksandr Kovalko on 07.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct GlucoseSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
